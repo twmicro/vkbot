2525dcb071afe8c6b436a5f3621bf09a2d2b3ff1
twmicro <chicherintim@gmail.com>
2018-09-16 22:50:46 +0300

    New functions added

--- VKBot.py
+++ VKBot.py
@@ -2,10 +2,59 @@ import vk_api
 import time
 import sys, traceback
 import datetime
-vk = vk_api.VkApi(token='c87370d527dea2ce349039c44a68f1901996ebdef8ae617c275c9c01e9adb64b0ccffd46f60cf5f6f4e49')
+vk = vk_api.VkApi(token='ebbe94d588065027e84301a672516f07e3a39a454a638332786e69d7b2f6759e76840654993bb5d5a5eec')
 vk._auth_token()
 
 values = {'out':0, 'count':100, 'time_offset':60, 'group_id':160615635}
+OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
+             '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+
+
+def eval_(formula):
+    def parse(formula_string):
+        number = ''
+        for s in formula_string:
+            if s in '1234567890.':
+                number += s
+            elif number:
+                yield float(number)
+                number = ''
+            if s in OPERATORS or s in "()":
+                yield s
+        if number:
+            yield float(number)
+
+    def shunting_yard(parsed_formula):
+        stack = []
+        for token in parsed_formula:
+            if token in OPERATORS:
+                while stack and stack[-1] != "(" and OPERATORS[token][0] <= OPERATORS[stack[-1]][0]:
+                    yield stack.pop()
+                stack.append(token)
+            elif token == ")":
+                while stack:
+                    x = stack.pop()
+                    if x == "(":
+                        break
+                    yield x
+            elif token == "(":
+                stack.append(token)
+            else:
+                yield token
+        while stack:
+            yield stack.pop()
+
+    def calc(polish):
+        stack = []
+        for token in polish:
+            if token in OPERATORS:
+                y, x = stack.pop(), stack.pop()
+                stack.append(OPERATORS[token][1](x, y))
+            else:
+                stack.append(token)
+        return stack[0]
+
+    return calc(shunting_yard(parse(formula)))
 def write_msg(user_id, s):
     vk.method('messages.send', {'user_id':user_id, 'message':s})
 
@@ -17,6 +66,7 @@ def wd_string():
 def now_mth():
     months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
     return months[datetime.datetime.now().month - 1]
+
 while True:
     try:
         response = vk.method('messages.getConversations', values)
@@ -39,9 +89,13 @@ while True:
                 answer = 'Пока'
             elif item['last_message']['text'] == "Как дела?":
                 answer = 'Хорошо'
-
+            elif item['last_message']['text'] == "Хей":
+                answer = 'Привет братан'
             else:
-                answer = 'Не понял :)'
+                try:
+                    answer = eval_(item['last_message']['text'])
+                except:
+                    answer = 'Не понял :)'
             write_msg(item['last_message']['from_id'], answer)
             #print(item['last_message']['from_id'])
             print(datetime.datetime.now(),'from id {} sent message = {} we answered = {}'.
