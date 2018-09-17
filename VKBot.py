11c8d98216fbc705d47b67de5cc9c06276b3aafd
twmicro <chicherintim@gmail.com>
2018-09-17 12:45:54 +0300

    Structured and comment VKBot.py

--- VKBot.py
+++ VKBot.py
@@ -1,26 +1,60 @@
+# Author: twmicro, chicherintim@gmail.com
+
+# imports
 import vk_api
 import time
 import sys, traceback
 import datetime
-vk = vk_api.VkApi(token='ebbe94d588065027e84301a672516f07e3a39a454a638332786e69d7b2f6759e76840654993bb5d5a5eec')
-vk._auth_token()
+
+# variables
 
 values = {'out':0, 'count':100, 'time_offset':60, 'group_id':160615635}
 OPERATORS = {'+': (1, lambda x, y: x + y), '-': (1, lambda x, y: x - y),
              '*': (2, lambda x, y: x * y), '/': (2, lambda x, y: x / y)}
+PI = 3.14
+CONFIG_VARIABLES = {'П':3.14}
 
+# authorize
 
+vk = vk_api.VkApi(token='c87370d527dea2ce349039c44a68f1901996ebdef8ae617c275c9c01e9adb64b0ccffd46f60cf5f6f4e49')
+vk._auth_token()
+
+# functions
+
+def write_msg(user_id, s):
+    vk.method('messages.send', {'user_id': user_id, 'message': s})
+
+def now_wd():
+    return datetime.datetime.now().weekday() + 1
+
+def wd_string():
+    days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
+    return days[now_wd() - 1]
+
+def now_mth():
+    months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь',
+              'Ноябрь', 'Декабрь']
+    return months[datetime.datetime.now().month - 1]
 def eval_(formula):
     def parse(formula_string):
         number = ''
+        index = 0
         for s in formula_string:
+            #for i in range(0, len(CONFIG_VARIABLES) - 1):
+            #    if s == CONFIG_VARIABLES.keys()[i]:
+            #       yield CONFIG_VARIABLES.values()[i]
             if s in '1234567890.':
                 number += s
+            elif s == 'П':
+               yield PI
+            elif s == 'И':
+                pass
             elif number:
                 yield float(number)
                 number = ''
             if s in OPERATORS or s in "()":
                 yield s
+            index+=1
         if number:
             yield float(number)
 
@@ -48,25 +82,15 @@ def eval_(formula):
         stack = []
         for token in polish:
             if token in OPERATORS:
-                y, x = stack.pop(), stack.pop()
+                y, x = stack.pop(), stack.pop()-0
                 stack.append(OPERATORS[token][1](x, y))
             else:
                 stack.append(token)
         return stack[0]
 
     return calc(shunting_yard(parse(formula)))
-def write_msg(user_id, s):
-    vk.method('messages.send', {'user_id':user_id, 'message':s})
 
-def now_wd():
-        return datetime.datetime.now().weekday() + 1
-def wd_string():
-    days = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье']
-    return days[now_wd() - 1]
-def now_mth():
-    months = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь']
-    return months[datetime.datetime.now().month - 1]
-
+# loop
 while True:
     try:
         response = vk.method('messages.getConversations', values)
